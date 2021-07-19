2be59f182305502b306bf48d985308566cea48d1
ennache <catalinenache03@gmail.com>
2021-07-19 15:37:26 +0300

    completed arrays

--- Day 0/index.js
+++ Day 0/index.js
@@ -15,9 +15,9 @@ console.log(WHITE_FOREGROUND, SCOPE_TEST)
 eval(SCOPE_TEST)
 console.log(GREEN_FOREGROUND, "After execution we get: ");
 
-try{ console.log(" var_variable is ",var_variable) } catch(e){console.warn(e)}
-try{ console.log(" let_variable is ",let_variable) } catch(e){console.warn(e)}
-try{ console.log(" const_variable is ",const_variable) } catch(e){console.warn(e)}
+try{ console.log(" var_variable is ", var_variable) } catch(e){console.warn(e)}
+try{ console.log(" let_variable is ", let_variable) } catch(e){console.warn(e)}
+try{ console.log(" const_variable is ", const_variable) } catch(e){console.warn(e)}
 
 console.log(GREEN_FOREGROUND,"*var variables can be referenced before declaration while the other two types can't")
 console.log(GREEN_FOREGROUND,"*all variable types are hoisted, the difference is that var is also initialised with undefined")
@@ -96,9 +96,81 @@ console.log("\n\n")
 
 
 //Arrays
+console.log(GREEN_FOREGROUND, "*****Array","\n\n")
+console.log(GREEN_FOREGROUND,"**Declaration and adressing")
+
+const DECLARATION_AND_ADRESSING_TEST = `
+let arr1 = [1,2,3,4]
+let arr2 = new Array(2,4,5,6)
+let arr3 = []
+let arr4 = new Array("zero_cell",5,"test")
+let matrix1 = [[1,2,3], [4,5,6]]
+let matrix2 = [arr1, arr2]
+
+console.log(arr4.zero_cell, arr4[1], arr4.length, matrix2)
+`
+console.log(WHITE_FOREGROUND, DECLARATION_AND_ADRESSING_TEST,"\n\n")
+eval(DECLARATION_AND_ADRESSING_TEST)
+
+console.log(GREEN_FOREGROUND, "**Iteration","\n\n")
+
+const ITERATION_TEST = `
+let arr = [6,2,6,8,3,57,8]
+
+let buffer = ""
+for (let x in arr){ buffer += (x + ", ") } //x represents the index
+console.log(buffer)
+
+buffer = ""
+for (let x = 0; x < arr.length; x++){ buffer += (arr[x]+", ") } //x represents the index
+console.log(buffer)
+
+buffer = ""
+arr.forEach(element => { buffer+= (element + ", ") })
+console.log(buffer)
+
+buffer = ""
+arr.map(element=> {buffer+=(element + ", "); return element}) //not recommended, map is misused
+console.log(buffer)
+`
+console.log(GREEN_FOREGROUND, "*Multiple ways of iterating through an array")
+console.log(WHITE_FOREGROUND, ITERATION_TEST)
+eval(ITERATION_TEST)
+console.log("\n\n")
 
 
 
+console.log(GREEN_FOREGROUND, "**Mutator methods in the context of Arrays","\n\n")
+
+console.log(GREEN_FOREGROUND, "*A mutator method is a method that affects the array directly opossing the non-mutator methods.")
+console.log(GREEN_FOREGROUND, "*Example: push, pop, shift, unshift, sort, reverse, forEach, splice and fill.")
+console.log(GREEN_FOREGROUND, "*A non-mutator method is a method that returns the new modified array, without touching the original array")
+console.log(GREEN_FOREGROUND, "*Example: join, filter, find, includes, map, slice, concat, every, findIndex, reduce, some, flat and flatMap");
+
+const MUTATOR_TEST = `
+let returned_value;
+let arr = [1,6,4,7,4]
+
+returned_value = arr.unshift(9)
+console.log(returned_value, arr)
+
+returned_value = arr.pop()
+console.log(returned_value, arr)
+
+returned_value = arr.sort()
+console.log(returned_value, arr)
+
+//non-mutator
+returned_value = arr.join(" >> ")
+console.log(returned_value, arr)
+
+returned_value = arr.includes(7)
+console.log(returned_value, arr)
+`
+
+console.log(WHITE_FOREGROUND, MUTATOR_TEST)
+eval(MUTATOR_TEST)
+console.log(GREEN_FOREGROUND, "*MUTATOR methods might return values")
 
 
 //Promise and Callbacks
